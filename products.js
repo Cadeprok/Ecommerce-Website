857eccbc21214ed6d43dc6ebd4f7f1d2427151b8
Cadeprok <prokopc1@montclair.edu>
2023-06-11 21:24:01 -0400

    Added array of objects for products.

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+
+    {
+        id: 0,
+        name:'Bootstrap Tutorial',
+        price: 30,
+        numPurchasable: 5,
+        description: "lorem8",
+        imgSource:"./img/Bootstrap_logo.png" 
+    },
+
+    {
+        id: 1,
+        name:'C# Tutorial',
+        price: 40,
+        numPurchasable: 5,
+        description: "lorem10",
+        imgSource: "./img/c-sharp-logo.png"
+    },
+    
+    {
+        id: 2,
+        name:'C++ Tutorial',
+        price: 40,
+        numPurchasable: 5,
+        description: "lorem9",
+        imgSource: "./img/C++_Logo.png"
+    },
+    
+    {
+        id: 3,
+        name:'Python Tutorial',
+        price: 40,
+        numPurchasable: 5,
+        description: "lorem10",
+        imgSource: "./img/Python.png"
+    },
+    
+    {
+        id: 4,
+        name:'Java Tutorial',
+        price: 40,
+        numPurchasable: 5,
+        description: "lorem10",
+        imgSource: "./img/Java-Logo.png"
+    },
+    {
+        id: 5,
+        name:'JavaScript Tutorial',
+        price: 40,
+        numPurchasable: 5,
+        description: "lorem8",
+        imgSource: "./img/js-logo.png"
+    }
+
+]
