f44ece904ff4e5d1ba6de5821c09ec3e3a2b18dd
Cadeprok <prokopc1@montclair.edu>
2023-06-11 22:14:22 -0400

    Edited renderProducts to work correctly (correct information and all information necessary)

--- main.js
+++ main.js
@@ -4,19 +4,30 @@ const productsElement = document.querySelector(".products");
 // Produces all products
 
 function renderProducts(){
-    products.forEach((product) => {
+    products.forEach((product) => { // need to align items to the center. 
         productsElement.innerHTML += `
-        <div class="card item">
+
+        <div class="card item" style="width: 30rem;"> 
             <img src="${product.imgSource}" alt="${product.name}" height="275">
             <div class="card-body">
                 <h5 class="card-title">
                     ${product.name}
                 </h5>
+                <h4>
+                    <b>$ ${product.price}</b>
+                </h4>
                 <p class="card-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum est quam corrupti iusto consequatur error, voluptatum a asperiores hic eum?
+                    ${product.description}
                 </p>
+                <br>
+                <div class="cartButton">
+                    <button class="b2">
+                        <h7 class="insideButton py-3 mx-3"><b>Add To Cart</b></h7>
+                    </button>
+                </div>
             </div>
         </div>
+        <br>
         `
     })
 }
