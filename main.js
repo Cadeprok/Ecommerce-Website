b6a4d577fe46c3ee501884d63a6fa4e198797154
Cadeprok <prokopc1@montclair.edu>
2023-06-11 22:16:04 -0400

    Unsaved edits which did not get included with prior commits --> button styling and small edit to main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function renderProducts(){
                 <br>
                 <div class="cartButton">
                     <button class="b2">
-                        <h7 class="insideButton py-3 mx-3"><b>Add To Cart</b></h7>
+                        <h7 class="insideButton py-3 mx-3"><b><a class="aInsideButton" href="">Add To Cart</a></b></h7>
                     </button>
                 </div>
             </div>
