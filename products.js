1efc7a05f1a35a6d92ad1baca7f25ecf0ee5bdda
Cadeprok <prokopc1@montclair.edu>
2023-06-14 20:37:35 -0400

    changed description in product list js file

--- products.js
+++ products.js
@@ -5,7 +5,7 @@ const products = [
         name:'Bootstrap Tutorial',
         price: 30,
         numPurchasable: 5,
-        description: "lorem8",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore iure dolorum recusandae, mollitia non corporis ex magni, qui in, totam eum. Incidunt delectus accusantium ea ullam sequi suscipit optio aliquid.",
         imgSource:"./img/Bootstrap_logo.png" 
     },
 
@@ -14,7 +14,7 @@ const products = [
         name:'C# Tutorial',
         price: 40,
         numPurchasable: 5,
-        description: "lorem10",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio autem quia possimus quis consequuntur nostrum. Corrupti, minima. Quidem nemo voluptates nihil necessitatibus consequuntur enim inventore nulla, laudantium minima sint a omnis delectus!",
         imgSource: "./img/c-sharp-logo.png"
     },
     
@@ -23,7 +23,7 @@ const products = [
         name:'C++ Tutorial',
         price: 40,
         numPurchasable: 5,
-        description: "lorem9",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. In eum repellat excepturi nulla eligendi amet expedita veritatis eius dolorum sint sapiente nostrum adipisci aperiam, soluta error.",
         imgSource: "./img/C++_Logo.png"
     },
     
@@ -32,7 +32,7 @@ const products = [
         name:'Python Tutorial',
         price: 40,
         numPurchasable: 5,
-        description: "lorem10",
+        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio ipsum, architecto ipsam voluptas nostrum hic maiores nobis perferendis deserunt expedita quaerat odit. Mollitia, quas.",
         imgSource: "./img/Python.png"
     },
     
@@ -41,7 +41,7 @@ const products = [
         name:'Java Tutorial',
         price: 40,
         numPurchasable: 5,
-        description: "lorem10",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, laboriosam quidem laudantium quae hic eligendi amet porro repellat harum, quia corporis? Doloribus facilis, perspiciatis sed veniam numquam architecto facere repudiandae voluptatem saepe quaerat voluptas.",
         imgSource: "./img/Java-Logo.png"
     },
     {
@@ -49,7 +49,7 @@ const products = [
         name:'JavaScript Tutorial',
         price: 40,
         numPurchasable: 5,
-        description: "lorem8",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, nostrum temporibus nobis repellendus molestiae sunt error, quaerat minima ipsa non architecto adipisci.",
         imgSource: "./img/js-logo.png"
     }
 
