66f2309424430eeb9ab5317fe9918256ceaedcdf
Cadeprok <prokopc1@montclair.edu>
2023-06-18 16:53:31 -0400

    fixed 375 media quiery and small addiition to style.css and main.js

--- temp.js
+++ temp.js
@@ -22,3 +22,57 @@ cartArray.forEach((item) => {
 })
 
 total1.innerHTML = `$${totalPrice.toFixed(2)}`;
+
+
+
+@media screen and (max-width: 820px){
+    .temp{
+        padding: 0 0px;
+    }
+    element.style{
+        width: 0rem;
+    }
+    .card-group .items{
+        padding-left: 0px;
+        padding-right: 5px;
+        align-items: start;
+    }
+    }
+
+
+    @media screen and (min-width: 281px) and (max-width: 900px){
+        .theCart .p-card-text{
+            font-size: 12px;
+        }
+        section{
+            padding: 0 0px;
+        }
+        .navbar{
+            width: 100%;
+        }
+        .banner{
+            width:100%;
+        }
+        img {
+            height: 200px;
+            width: 70%;
+        }
+        .card-body{ 
+            padding: 0rem;
+            font-size: 12px;
+            padding-right: 110px;
+            padding-left: 10px;
+        }
+        .temp{
+            display: list-item;
+        }
+        .cart-body .temp1{
+            background-color: rgb(89, 151, 231);;
+            height: 175px;
+            width: 80%;
+        }
+        element.style {
+            font-size: 20px;
+            padding-right: 15px;
+        }
+    }
